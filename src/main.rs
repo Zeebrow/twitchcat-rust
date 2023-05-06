aa7e7a328e032721604c2337757c64bfcd51e282
Zeebrow <mzborowski@yahoo.com>
2023-05-05 22:16:04 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
 mod bot;
-use crate::bot::{Bot, TwitchChannel};
+mod config;
+use crate::bot::Bot;
+use crate::config::TwitchBotConfig;
 use std::env;
-
+use log::*;
+use env_logger;
+use twitch_api::{helix::{self, Request, RequestGet, users::{GetUsersRequest, User}}, types, TwitchClient, twitch_oauth2::AppAccessToken};
+use twitch_api::helix::streams::get_streams;
+use twitch_api::twitch_oauth2::{Scope, ClientId, ClientSecret};
+use reqwest;
 
 fn main() -> Result<(), anyhow::Error> {
+    // env_logger::init();
+    // let client_id: ClientId = ClientId::new(env::var("TWITCH_BOT_CLIENT_ID").unwrap());
+    // let client_secret: ClientSecret = ClientSecret::new(env::var("TWITCH_BOT_CLIENT_SECRET").unwrap());
+    // let scopes: Vec<Scope> = vec![];
+    // let token = AppAccessToken::get_app_access_token(&client, client_id, client_secret, scopes);
+
+    // let logins: &[&types::UserNameRef] = &["justintv123".into()];
+    // let request = GetUsersRequest::logins(logins);
+    // let client: TwitchClient<reqwest::Client> = TwitchClient::default();
+    // let response = send_http_request(request.create_request("accesstoken", "client_id")?)?;
+
+
+    // let users = &["justintvfan".into()];
+    // let request = get_streams::GetStreamsRequest::builder()
+    //     .user_login(users[..])
+    //     .build();
+    // panic!("asdf");
+    
+    let app_config = TwitchBotConfig::get_config();
+    dbg!(&app_config);
     let args: Vec<String> = env::args().collect();
     println!("{:#?}", args);
-    let mut b = Bot::new(None);
-    let channel_name = "lol_nemesis";
-    b.add_channel(TwitchChannel::new(String::from(channel_name), None)).unwrap_or_else(|e| {
-        println!("{} {}", e, channel_name)
-    });
-    b.run()
-
+    /* note to self: doesn't need to be mutable, perhaps because Bot manages its
+     * own memory? Let's go with that. */
+    let b = Bot::new(None);
+    let res = b.run();
+    println!("exiting program");
+    res
 }
